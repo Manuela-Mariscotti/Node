dd64b677486454cdf3f27dca43509c3562f7a59a
Manuela-Mariscotti <manuela.mariscotti@gmail.com>
2022-08-09 14:09:24 +0200

    modificacion en parametro readConsole()

--- proyecto_node/index.js
+++ proyecto_node/index.js
@@ -2,8 +2,6 @@ let myLib = require("./writeAndReadObject");
 let myLib2 = require("./readConsole");
 
 
-console.log(myLib.writeAndRead("myObj2.json",{"name":"Nestor","surname":"Kirchner","age":"70"}));
+// console.log(myLib.writeAndRead("myObj2.json",{"name":"Nestor","surname":"Kirchner","age":"70"}));
 
-myLib2.readConsole()
-
-
+myLib2.readConsole("persona.json");

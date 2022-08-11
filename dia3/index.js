291be3b4c9b543050bdb3e9fb88d1c1ac469c427
Manuela-Mariscotti <manuela.mariscotti@gmail.com>
2022-08-11 09:39:51 +0200

    .

--- dia3/index.js
+++ dia3/index.js
@@ -1,21 +1,21 @@
 const express = require ('express');
 const app = express();
 
-// app.get('/miCasa',function(req,res){
+app.get('/miCasa',function(req,res){
 
-//     console.log('Request received from client');
-//     console.log('Request URL : '+req.url);
-//     console.log('Request Method : '+req.method);
-//     console.log('User-agent : '+req.headers["user-agent"]);
-//     res.writeHead(200,{'Content-Type':'application/json'});
-//     res.write(JSON.stringify(req.query));
+    console.log('Request received from client');
+    console.log('Request URL : '+req.url);
+    console.log('Request Method : '+req.method);
+    console.log('User-agent : '+req.headers["user-agent"]);
+    res.writeHead(200,{'Content-Type':'application/json'});
+    res.write(JSON.stringify(req.query));
     
-//     res.end();
+    res.end();
 
-// })
-// app.listen(3000);
+})
+app.listen(3000);
 
-app.post('/index',function(req,res){
+app.post('/',function(req,res){
     console.log('Request received from client');
     console.log('Request URL : '+req.url);
     console.log('Request Method : '+req.method);

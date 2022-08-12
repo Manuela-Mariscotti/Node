831a5a9255fdaf2926cedab854870d1c0b29e131
Manuela-Mariscotti <manuela.mariscotti@gmail.com>
2022-08-12 20:58:35 +0200

    retos dia4 terminados

--- dia4/src/controller/pros.controller.js
+++ dia4/src/controller/pros.controller.js
@@ -1,51 +1,118 @@
 const { Professional } = require("../../models/professional.model");
 const app = require("../app");
 
-// let pros =[]; 
-let pros =[null]; 
+let RobertDeNiro = new Professional ("Robert DeNiro","80","male","90", "160","black","brown","white","false","american","2","actor, producer");
+
+let SamuelLJackson = new Professional ("Samuel L. Jackson","65","male","90","160","black","brown","black","false","american","0","actor, producer");
+
+let TimRoth = new Professional ("Tim Roth","65","male","90","160","lightbrown","green","white","false","american","0","actor, producer");
+
+let pros = [RobertDeNiro, SamuelLJackson, TimRoth]
 
 function getPros(request,response){ 
     console.log('Lanzando la funcion getPros');
-    // let name = request.query.name;
-    // let respuesta;
-    // if(pros !=null && (!name || name ===pros.name))
-  
+    let respuesta;
+    let index = request.query.index
+    respuesta = {
+        error: false,
+        codigo: 200,
+        mensaje: "Profesional encontrado",
+        resultado: pros[index]
+    }
+    if(!index){
+        respuesta = {
+        error: false,
+        codigo: 200,
+        mensaje: "Profesionales encontrados",
+        resultado: pros
+        }
+    }
+    response.send(respuesta);
 }
 
 function postPros(request,response){
     console.log('Lanzando la funcion postPros');
     let respuesta;
     console.log(request.body);
-    if(pros===null){
-        pros.push(new Professional(
-            request.body.name,
-            request.body.age,
-            request.body.genre,
-            request.body.weight,
-            request.body.height,
-            request.body.hairColor,
-            request.body.eyeColor,
-            request.body.race,
-            request.body.isRetired,
-            request.body.nacionality,
-            request.body.oscarNumber,
-            request.body.profession
-        ))
-        respuesta = {
-            error: false,
-            codigo: 200,
-            mensaje: "Profesional creado",
-            resultado: pros
-        }
-    }else{
-        respuesta = {
-            error: true,
-            codigo: 200,
-            mensaje: "Profesional ya existe",
-            resultado: null
+    let pros1 = new Professional(
+        request.body.name,
+        request.body.age,
+        request.body.genre,
+        request.body.weight,
+        request.body.height,
+        request.body.hairColor,
+        request.body.eyeColor,
+        request.body.race,
+        request.body.isRetired,
+        request.body.nacionality,
+        request.body.oscarNumber,
+        request.body.profession
+    )
+    pros.push(pros1)
+    respuesta = {
+        error: false,
+        codigo: 200,
+        mensaje: "Profesional creado",
+        resultado: pros
+    }
+    response.send(respuesta);
+}
+
+function putPros(request,response){
+    console.log('Lanzando la funcion putPros');
+    let respuesta;
+    let nameInicial;
+    let nameNuevo=request.body.name
+    
+    for (let i = 0; i < pros.length; i++) {
+        nameInicial = pros[i].name;
+
+        if(nameNuevo == nameInicial){
+            console.log( nameInicial);
+            console.log(nameNuevo);
+            pros[i].name = request.body.name;
+            pros[i].age = request.body.age;
+            pros[i].genre = request.body.genre;
+            pros[i].weight = request.body.weight;
+            pros[i].height = request.body.height;
+            pros[i].hairColor = request.body.hairColor;
+            pros[i].eyeColor = request.body.eyeColor;
+            pros[i].race = request.body.race;
+            pros[i].isRetired = request.body.isRetired;
+            pros[i].nacionality = request.body.nacionality;
+            pros[i].oscarNumber = request.body.oscarNumber;
+            pros[i].profession = request.body.profession;
+            respuesta = {
+                error: false,
+                codigo: 200,
+                mensaje: "Profesional actualizado",
+                resultado: pros[i],
+            }
         }
     }
     response.send(respuesta);
 }
 
-module.exports = {getPros,postPros}
+function deletePros(request,response){
+    console.log('Lanzando la funcion putPros');
+    let respuesta;
+    let nameInicial;
+    let nameNuevo=request.body.name
+    
+    for (let i = 0; i < pros.length; i++) {
+        nameInicial = pros[i].name;
+
+        if(nameNuevo == nameInicial){
+            pros.splice(i,1)
+            respuesta = {
+                error: false,
+                codigo: 200,
+                mensaje: "Profesional eliminado",
+                resultado: pros,
+            }
+        }
+    }
+    response.send(respuesta);
+}
+
+module.exports = {getPros, postPros, putPros, deletePros}
--- dia4/src/routers/pros.routers.js
+++ dia4/src/routers/pros.routers.js
@@ -1,9 +1,13 @@
 const {Router} = require("express");
 const router = Router();
-const prosCtrl = ("../controller/pros.controller");
+const prosCtrl = require ("../controller/pros.controller");
 
-router.get("./pros", prosCtrl.getPros)
+router.get("/pros", prosCtrl.getPros)
 
-router.post("./pros", prosCtrl.postPros)
+router.post("/pros", prosCtrl.postPros)
+
+router.put("/pros", prosCtrl.putPros)
+
+router.delete("/pros",prosCtrl.deletePros)
 
 module.exports = router;
